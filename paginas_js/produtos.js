ce3b76ba8057feafbd31e94eacf8fa808009db5c
matheus <matheus.mmn@hotmail.com>
2022-02-11 14:45:34 -0300

    responsividade

--- paginas_js/produtos.js
+++ paginas_js/produtos.js
@@ -1,43 +1,71 @@
-produtos = {
-    'Água Mineral' : {
-        'descrição': 'Água Mineral',
-        'valor': 2.00
-    },
+// produtos = {
+//     'Água Mineral' : {
+//         'descrição': 'Água Mineral',
+//         'valor': 2.00
+//     },
 
-    'Guaraná 350 ml' : {
-        'descrição': 'Guaraná 350 ml',
-        'valor': 2.50
-    },
+//     'Guaraná 350 ml' : {
+//         'descrição': 'Guaraná 350 ml',
+//         'valor': 2.50
+//     },
 
-    'Redbull' : {
-        'descrição': 'Redbull',
-        'valor': 7.35
-    }
-}
+//     'Redbull' : {
+//         'descrição': 'Redbull',
+//         'valor': 7.35
+//     }
+// }
 
-var indexes = []
+// var indexes = []
 
-for (const property in produtos) {
-    indexes.push(property)
-}
+// for (const property in produtos) {
+//     indexes.push(property)
+// }
 
-console.log(indexes)
+// console.log(indexes)
 
-indexes.forEach(function(item){
-    $('#cod').append('<option>' + item + '</option>');
-});
+// indexes.forEach(function(item){
+//     $('#cod').append('<option>' + item + '</option>');
+// });
 
-$('#cod').change(function() {
-    var option = $('#cod').find(":selected").text();
-    console.log(option)
+// $('#cod').change(function() {
+//     var option = $('#cod').find(":selected").text();
+//     console.log(option)
 
-    $("#des").val(produtos[option]['descrição'])
-    $("#vun").val('R$ ' + produtos[option]['valor'])
+//     $("#des").val(produtos[option]['descrição'])
+//     $("#vun").val('R$ ' + produtos[option]['valor'])
 
-    $('#qtd').keyup(function(){
-        var qtd = $(this).val()
-        var total = Number(produtos[option]['valor']) * Number(qtd)
-        $("#tot").val('R$ ' + total)
+//     $('#qtd').keyup(function(){
+//         var qtd = $(this).val()
+//         var total = Number(produtos[option]['valor']) * Number(qtd)
+//         $("#tot").val('R$ ' + total)
 
+//     });
+// });
+
+$.get("http://127.0.0.1:8000/index/", function(resultado){
+
+    resultado.forEach(function(item){
+        $('#cod').append('<option>' + item.nome + '</option>');
     });
-});
+    
+
+    $('#cod').change(function() {
+        var option = $('#cod').find(":selected").index()
+
+        var db = option - 1
+    
+        $("#des").val(resultado[db].nome)
+        $("#vun").val('R$ ' + resultado[db].registro)
+    
+        $('#qtd').keyup(function(){
+            var qtd = $(this).val()
+            var total = Number(resultado[db]['registro']) * Number(qtd)
+            $("#tot").val('R$ ' + total)
+    
+        });
+    });
+
+
+
+
+})
