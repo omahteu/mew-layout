41d0e7ddf568adb0d347f2467b7f96c723cc0fb1
matheus <matheus.mmn@hotmail.com>
2022-02-11 18:26:37 -0300

    ajax

---
+++ ajaxs/getProduto.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $.get("http://127.0.0.1:8000/cadastroproduto/", function(resultado){
+
+    resultado.forEach(function(item){
+        $('#cod').append('<option>' + item.descricao + '</option>');
+    });
+    
+
+    $('#cod').change(function() {
+        var option = $('#cod').find(":selected").index()
+
+        var db = option - 1
+    
+        $("#des").val(resultado[db].descricao)
+        $("#vun").val('R$ ' + resultado[db].valorunitario)
+    
+        $('#qtd').keyup(function(){
+            var qtd = $(this).val()
+            var total = Number(resultado[db]['valorunitario']) * Number(qtd)
+            $("#tot").val('R$ ' + total)
+        });
+    });
+})
+})
+
+
--- ajaxs/postProduto.js
+++ ajaxs/postProduto.js
@@ -1,23 +1,40 @@
-$("#enviar").click(function(){
+$("#salvar").click(function(){
     
     let codigo = $("#codigo").val()
     let descricao = $("#descricao").val()
     let valor = $("#valor").val()
     let categoria = $("#categoria").val()
-    let data = $("#data").val()
 
+    var horaEntrada = new Date();
+    var hora = horaEntrada.getHours()
+    var minutos = horaEntrada.getMinutes()
+
+    let horaAtual = String(hora) + ':' + String(minutos)
+
+    console.log(codigo)
 
     var dados = {
-        codigo: codigo,
-        descricao: descricao,
-        valorunitario: valor,
-        categoria: categoria,
-        data: data
-    }
+            codigo: codigo,
+            descricao: descricao,
+            valorunitario: valor,
+            categoria: categoria,
+            data: horaAtual
+        }
+    
 
-    $.ajax({
-        method: "POST",
-        url: "http://127.0.0.1:8000/cadastroproduto/",
-        data: dados
+    // $.ajax({
+    //     method: "POST",
+    //     url: "http://127.0.0.1:8000/cadastroproduto/",
+    //     data: 
+    // })
+
+    $.post("http://127.0.0.1:8000/cadastroproduto/", dados, function(msg){
+        alert("Produto Registrado!")
+
+        document.getElementById('formPostProduto').reset()
     })
+
+    // alert("Produto Registrado!")
+
+    // document.getElementById('formPostProduto').reset()
 })
