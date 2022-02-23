7c3beeb826a937d2bab0b91a41a45c59039b1cb7
matheus <matheus.mmn@hotmail.com>
2022-02-23 16:31:25 -0300

    ajax javascript

--- ajaxs/getIg.js
+++ ajaxs/getIg.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $.get("http://127.0.0.1:8000/igs/", function(resultado){
+    /*$.get("http://127.0.0.1:8000/igs/", function(resultado){
 
         var endereco = resultado[0].endereco + ' ' + ' ' + resultado[0].numero + ' - ' + resultado[0].bairro + ' , ' + 
         resultado[0].cidade
@@ -12,5 +12,27 @@ $(document).ready(function(){
         $("#telefone").text(resultado[0].telefone)
         $("#telefone2").text(resultado[0].telefone2)
         $("#telefone3").text(resultado[0].telefone3)
-    })
+    })*/
+
+    const dados = new XMLHttpRequest()
+    dados.onload = function(){
+        
+
+        var resultado = JSON.parse(dados.responseText)
+        console.log(resultado[0])
+
+        var endereco = resultado[0].endereco + ' ' + ' ' + resultado[0].numero + ' - ' + resultado[0].bairro + ' , ' + 
+        resultado[0].cidade
+
+
+        $("#email").text(resultado[0].fantasia)
+        $("#endereco").text(endereco)
+        $("#telefone").text(resultado[0].telefone)
+        $("#telefone2").text(resultado[0].telefone2)
+        $("#telefone3").text(resultado[0].telefone3)
+    }
+
+    dados.open("GET", "http://127.0.0.1:8000/igs/", true)
+    dados.send()
 })
+
--- botoes/botaoFinalizar.js
+++ botoes/botaoFinalizar.js
@@ -3,6 +3,26 @@ $("#finalizar").click(function(){
     var numero_quarto = JSON.parse(sessionStorage.getItem('Encerrando'))
     var codigo_quarto = JSON.parse(localStorage.getItem("quarto" + numero_quarto))
     var dados_head = JSON.parse(localStorage.getItem(codigo_quarto))
+    var dados_produto = JSON.parse(localStorage.getItem('produtos'))
 
-    console.log(dados_head)
+    //Sconsole.log(codigo_quarto)
+    //console.log(dados_head)
+    //
+
+    //localStorage.removeItem(codigo_quarto) 
+    //localStorage.removeItem("quarto" + numero_quarto) 
+
+    //var dados = dados_produto.filter(quartos => quartos.quarto == String("quarto" + numero_quarto))
+
+    
+
+    
+
+    dados_produto.forEach(function(quarto){
+        console.log(quarto)
+    })
+
+    let aluno = dados_produto.find(function(aluno) {
+        return aluno.quarto === '2';
+      });
 })
